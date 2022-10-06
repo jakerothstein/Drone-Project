f220db12e85da11a0cb3ea4f55a37d9955a7bb65
Jake R <73565590+jakerothstein@users.noreply.github.com>
2022-10-05 17:33:12 -0700

    motion tracking beta

--- main.py
+++ main.py
@@ -1,46 +1,34 @@
-print 'Start Script'
-for chan in range(1,9):
-    Script.SendRC(chan,1500,False)
-    Script.SendRC(3,Script.GetParam('RC3_MIN'),True)
-
-Script.Sleep(5000)
-
-while cs.lat == 0:
-    print 'Waiting for GPS'
-    Script.Sleep(1000)
-
-print 'Got GPS'
-Script.SendRC(3,1000,False)
-Script.SendRC(4,2000,True)
-cs.messages.Clear()
-Script.Sleep(5000)
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
 
 
+def motionDetection():
+    cap = cv.VideoCapture(0)
+    ret, frame1 = cap.read()
+    ret, frame2 = cap.read()
+    while cap.isOpened():
+        diff = cv.absdiff(frame1, frame2)
+        diff_gray = cv.cvtColor(diff, cv.COLOR_BGR2GRAY)
+        blur = cv.GaussianBlur(diff_gray, (5, 5), 0)
+        _, thresh = cv.threshold(blur, 20, 255, cv.THRESH_BINARY)
+        dilated = cv.dilate(thresh, None, iterations=3)
+        contours, _ = cv.findContours(dilated, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+        for contour in contours:
+            (x, y, w, h) = cv.boundingRect(contour)
+            if cv.contourArea(contour) < 900:
+                continue
+            cv.rectangle(frame1, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv.putText(frame1, "Movement Detected", (10, 20), cv.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), 3)
+        cv.imshow("DroneView", frame1)
+        frame1 = frame2
+        ret, frame2 = cap.read()
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv.destroyAllWindows()
 
 
-# # def get_info():
-# #     armed = cs.armed
-# #     return [armed]
-#
-# print('Start Script')
-#
-# for chan in range(1, 9):
-#     Script.SendRC(chan, 1500, False)
-#     Script.SendRC(3, Script.GetParam('RC3_MIN'), True)
-#
-# Script.Sleep(5000)
-#
-# while cs.lat == 0:
-#     print('Waiting for GPS')
-#     Script.Sleep(1000)
-#
-# print('Got GPS')
-# Script.SendRC(3, 1000, False)
-# print("hi")
-# Script.SendRC(4, 2000, True)
-# print("hi")
-# cs.messages.Clear()
-# print("hi")
-# Script.SendRC(4, 1500, True)
-# print('Motors Armed!')
-# Script.SendRC(3, 1700, True)
+if __name__ == "__main__":
+    motionDetection()
